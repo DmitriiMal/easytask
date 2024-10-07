6b7347a9d4bd941005cffd070a9c2e79478130c9
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-07 22:38:43 +0200

    added custom event with @Output and $event variable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  computed,
+  Input,
+  Output,
+  input,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +14,24 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  // Signal Method
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
