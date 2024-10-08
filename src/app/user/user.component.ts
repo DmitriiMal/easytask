b6750e4c6bb2e4491ce18fed4ddcd285299a096d
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-08 17:55:11 +0200

    retrieve an object with @Input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,13 +15,18 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   // Signal Method
@@ -34,6 +39,6 @@ export class UserComponent {
   // });
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
