d156eb024609b72ed1ba285ce64400bca2fc9c5c
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-09 23:16:46 +0200

    styled selected user button dynamicaly

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -21,6 +21,7 @@ export class UserComponent {
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
