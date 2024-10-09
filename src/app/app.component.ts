3382d72938b754b800c3b07fcba802f3104f49ae
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-09 10:04:03 +0200

    created and styled task; and listed the tasks depending on users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, TaskComponent],
   styleUrl: './app.component.css',
   templateUrl: './app.component.html',
 })
